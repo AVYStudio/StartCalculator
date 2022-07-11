e61ff382d1b174303b2b26a493c985688517e9d6
AVYStudio <aveprev626@gmail.com>
2022-07-12 00:13:48 +0300

    intent calculator

--- app/src/main/java/ru/veprev/startcalculator/MainActivity.java
+++ app/src/main/java/ru/veprev/startcalculator/MainActivity.java
@@ -2,7 +2,11 @@ package ru.veprev.startcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button move = findViewById(R.id.move);
+
+        move.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("calculator://launch"));
+            startActivity(intent);
+        });
     }
 }
